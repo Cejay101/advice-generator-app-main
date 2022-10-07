14b3cdec235fdcf35ba285a4b8e3a4ef4633fb76
cejay101 <clintonjoyfimie@gmail.com>
2022-10-07 10:37:36 +0100

    jsCommit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict'
+const adviceNum = document.querySelector(".advice-number");
+const adviceText = document.querySelector(".advice-text");
+const nextAdvice = document.querySelector(".shine");
+
+const  getAdvice =async()=>{
+const res = await fetch("https://api.adviceslip.com/advice")
+const data =await res.json()
+
+adviceText.innerHTML =`"${data.slip.advice}"`
+}
+getAdvice()
+nextAdvice.addEventListener('click', function(){
+  // Increase the advice number
+adviceNum.textContent = (+adviceNum.textContent+1);
+adviceText.textContent= getAdvice()
+
+})
