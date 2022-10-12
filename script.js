1cc223b1f0c75cc3ed282d3dd9e8c962e2891da0
cejay101 <clintonjoyfimie@gmail.com>
2022-10-12 05:06:45 +0100

    coding

--- script.js
+++ script.js
@@ -2,17 +2,17 @@
 const adviceNum = document.querySelector(".advice-number");
 const adviceText = document.querySelector(".advice-text");
 const nextAdvice = document.querySelector(".shine");
-
+// Advice API
 const  getAdvice =async()=>{
 const res = await fetch("https://api.adviceslip.com/advice")
 const data =await res.json()
 
 adviceText.innerHTML =`"${data.slip.advice}"`
 }
-getAdvice()
-nextAdvice.addEventListener('click', function(){
-  // Increase the advice number
-adviceNum.textContent = (+adviceNum.textContent+1);
-adviceText.textContent= getAdvice()
+// getAdvice()
+// nextAdvice.addEventListener('click', function(){
+//   // Increase the advice number
+// adviceNum.textContent = (+adviceNum.textContent+1);
+// adviceText.textContent= getAdvice()
 
-})
+// })
